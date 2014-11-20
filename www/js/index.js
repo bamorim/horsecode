6a3cd7248d450ce96bc4bee68650a2aca3565922
Bernardo Amorim <contato@bamorim.com>
2014-11-20 19:06:02 -0200

    Fair enough

--- www/js/index.js
+++ www/js/index.js
@@ -17,80 +17,108 @@
  * under the License.
  */
 var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        var current = 0,
-            longTimeout,
-            outTimeout,
-            spaceTimeout,
-            char = [],
-            received = [];
-        var socket = io.connect("http://horsecode.herokuapp.com/");
-        document.addEventListener("touchstart",function(){
-            navigator.vibrate(100);
-            current = 1;
-            longTimeout = window.setTimeout(function(){
-                current = 2;
-                navigator.vibrate(100);
-            },400);
-            window.clearTimeout(outTimeout);
-            window.clearTimeout(spaceTimeout);
-        });
-        document.addEventListener("touchend",function(){
-            window.clearTimeout(longTimeout);
-            char.push(current);
-            outTimeout = window.setTimeout(function(){
-                socket.emit('message',char);
-                document.getElementById("e").innerHTML = char.toString();
-            },2000);
-            spaceTimeout = window.setTimeout(function(){
-                navigator.vibrate(100);
-                char.push(3);
-            },500);
+  // Application Constructor
+  initialize: function() {
+    this.bindEvents();
+  },
+  // Bind Event Listeners
+  //
+  // Bind any events that are required on startup. Common events are:
+  // 'load', 'deviceready', 'offline', and 'online'.
+  bindEvents: function() {
+    document.addEventListener('deviceready', this.onDeviceReady, false);
+  },
+  // deviceready Event Handler
+  //
+  // The scope of 'this' is the event. In order to call the 'receivedEvent'
+  // function, we must explicitly call 'app.receivedEvent(...);'
+  onDeviceReady: function() {
+    var current = 0,
+    longTimeout,
+    outTimeout,
+    spaceTimeout,
+    playInterval,
+    char = [],
+    received = [];
+    var socket = io.connect("http://horsecode.herokuapp.com/");
+    document.addEventListener("touchstart",function(){
+      // Pause the auto-play
+      pause();
 
-        });
-        socket.on('message',function(message){
-            var msg = char;
-            function vibrate(){
-                if(char.length == 0) return;
-                var pulse = char.pop();
-                if(pulse == 0)
-                    setTimeout(vibrate,500);
-                if(pulse == 1){
-                    navigator.vibrate(100);
-                    setTimeout(vibrate,200);
-                }if(pulse == 2){
-                    navigator.vibrate(400);
-                    setTimeout(vibrate,500);
-                }
-            }
-            vibrate();
-        });
-        app.receivedEvent('deviceready');
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement    = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement  = parentElement.querySelector('.received');
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-        console.log('Received Event: ' + id);
+      navigator.vibrate(100);
+      current = 0;
+      longTimeout = window.setTimeout(function(){
+        current = 1;
+        navigator.vibrate(100);
+      },400);
+
+      // Stop the touch-out timeouts
+      window.clearTimeout(outTimeout);
+      window.clearTimeout(spaceTimeout);
+    });
+    document.addEventListener("touchend",function(){
+      window.clearTimeout(longTimeout);
+      char.push(current);
+      outTimeout = window.setTimeout(function(){
+        if(char[char.length-1]==2) char.pop();
+        socket.emit('over',char);
+        char = [];
+
+        // Resume the auto-play
+        resume();
+      },2000);
+      spaceTimeout = window.setTimeout(function(){
+        navigator.vibrate(100);
+        char.push(2);
+      },500);
+
+    });
+
+    function pause(){
+      clearInterval(playInterval);
     }
+    function resume(){
+      pause();
+      playInterval = setInterval(play,50);
+    }
+    function play(){
+      if(received.length == 0) return;
+
+      clearInterval(play,50);
+      var message = received.shift();
+      function vibrate(){
+        if(message.length == 0){
+          setTimeout(resume,500);
+          return;
+        }
+        var pulse = message.shift();
+        if(pulse == 2)
+          setTimeout(vibrate,500);
+        if(pulse == 0){
+          navigator.vibrate(150);
+          setTimeout(vibrate,300);
+        }if(pulse == 1){
+          navigator.vibrate(450);
+          setTimeout(vibrate,600);
+        }
+      }
+      vibrate();
+    }
+
+    socket.on('copy',function(message){
+      received.push(message);
+    });
+    app.receivedEvent('deviceready');
+  },
+  // Update DOM on a Received Event
+  receivedEvent: function(id) {
+    var parentElement    = document.getElementById(id);
+    var listeningElement = parentElement.querySelector('.listening');
+    var receivedElement  = parentElement.querySelector('.received');
+    listeningElement.setAttribute('style', 'display:none;');
+    receivedElement.setAttribute('style', 'display:block;');
+    console.log('Received Event: ' + id);
+  }
 };
 
 app.initialize();
